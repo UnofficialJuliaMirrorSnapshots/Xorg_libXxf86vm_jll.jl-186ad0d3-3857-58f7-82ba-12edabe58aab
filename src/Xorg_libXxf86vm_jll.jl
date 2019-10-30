6524452e9e402393f7675f486aa346e8f0ebc50c
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-30 17:55:34 +0000

    Snapshot of branch master taken on 2019-10-30T13:55:33.54-04:00 from "https://github.com/UnofficialJuliaMirror/Xorg_libXxf86vm_jll.jl-186ad0d3-3857-58f7-82ba-12edabe58aab"

---
+++ src/Xorg_libXxf86vm_jll.jl
@@ -0,0 +1,31 @@
+module Xorg_libXxf86vm_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("186ad0d3-3857-58f7-82ba-12edabe58aab"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Xorg_libXxf86vm", artifacts_toml) for e in artifacts["Xorg_libXxf86vm"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Xorg_libXxf86vm; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Xorg_libXxf86vm_jll
